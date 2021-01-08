43d4adb2b3d8e86eda7d82ae721fcb289449e11a
margemcm <mmarshall@boomtownroi.com>
2021-01-08 08:48:39 -0500

    add script

---
+++ hello-world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# a simple hello world script
+
+
+def say_hello(name: str):
+    print("hello " + name + "!")
+
+def main():
+    say_hello("nance")
+
+
+if __name__ == '__main__':
+    main()
